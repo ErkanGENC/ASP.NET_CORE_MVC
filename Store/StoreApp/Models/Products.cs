31a90cba06253cfcc95aac988f7b05aa09be329a
Erkan Genc <gencerkan149@gmail.com>
2025-02-13 16:47:50 +0300

    .

---
+++ Store/StoreApp/Models/Products.cs
@@ -0,0 +1,10 @@
+namespace StoreApp.Models
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; } = string.Empty;
+       
+        public decimal Price { get; set; }
+    }
+}
